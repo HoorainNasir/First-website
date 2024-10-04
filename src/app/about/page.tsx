8e644c515c0e27c6361efab8e7df541b0276179d
HoorainNasir <hoorain.nasir001@gmail.com>
2024-10-04 19:38:10 +0500

    first commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -11,7 +11,7 @@ export default function About(){
                     <Image src="https://t3.ftcdn.net/jpg/07/06/10/62/360_F_706106273_azeLKRtE7CHga5JjHNfWAbgr4IkhEuwD.jpg" alt="profile picture" width={500} height={300} className="dp"/>
                 </div>
                 <div className="textContainer">
-                    <h1><strong>Hello, I'm Hoorain</strong></h1>
+                    <h1><strong>Hello, I am Hoorain</strong></h1>
                     <p>An intermediate-level student at GIAIC, actively developing my skills in HTML, CSS, Artificial Intelligence, Web3, and the Metaverse. I am passionate about learning emerging technologies and exploring innovative opportunities in the tech field.</p>
                 </div>
             </div>
